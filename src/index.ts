61caf5a9a5e8f00e2861c2b7746609cd375a4179
signe <signe.lindstrom@hotmail.com>
2023-10-12 15:18:13 +0200

    added express + helmet, cors, compression

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-const http = require('http');
 
+import * as express from 'express';
+import helmet from 'helmet';
+import * as cors from 'cors';
+import * as compression from 'compression';
+
+const app = express();
 const hostname = process.env.NODE_HOSTNAME;
 const port = process.env.NODE_PORT;
 
-const server = http.createServer((req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+app.use(helmet());
+app.options('*', cors({credentials: true, origin: true}));
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) =>{
+    // res.status(200);
     res.end('hej');
 });
 
-server.listen(port, hostname, () =>{
-    console.log(`server running at http://${hostname}:${port}/`);
+app.listen(port, hostname, () =>{
+    console.log(`[EXPRESS] running at http://${hostname}:${port}/`);
 })
